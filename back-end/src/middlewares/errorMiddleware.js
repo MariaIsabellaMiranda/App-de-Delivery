9e074e25dbea2592dd6df0960b9ce65bf0ce37b4
Pedro Reis <pedronr0309@gmail.com>
2022-10-31 17:24:17 -0300

    refactor: refatora middleware de erro

--- back-end/src/middlewares/errorMiddleware.js
+++ back-end/src/middlewares/errorMiddleware.js
@@ -4,21 +4,20 @@ const { JsonWebTokenError } = require('jsonwebtoken');
 const errorMiddleware = (err, _req, res, _next) => {
   console.error(err);
 
-  if (err instanceof MainError) {
-    return res.status(err.status).json({
-      message: err.message
-    });
+  switch(true) {
+    case err instanceof MainError:
+      return res.status(err.status).json({
+        message: err.message
+      });
+    case err instanceof JsonWebTokenError:
+      return res.status(401).json({
+        message: 'Token must be a valid token',
+      });
+    default:
+      return res.status(500).json({
+        message: 'Internal Error',
+      });
   }
-
-  if (err instanceof JsonWebTokenError) {
-    return res.status(401).json({
-      message: 'Token must be a valid token',
-    });
-  }
-
-  return res.status(500).json({
-    message: 'Internal Error',
-  });
 };
 
 module.exports = errorMiddleware;
