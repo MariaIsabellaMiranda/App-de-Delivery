ec00d62ffe0bc481f533dadb0918e40169e438f8
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-11-01 19:44:14 -0300

    FEAT: Adiciona requisito 18

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -1,6 +1,23 @@
+import CheckoutTable from '../../components/CheckoutTable';
+import Header from '../../components/Header';
+import { getTotalPrice } from '../../helpers/managerCart';
+import priceFormat from '../../helpers/priceFormat';
+
 function Checkout() {
   return (
-    <span>Checkout</span>
+    <>
+      <Header />
+      <span>Checkout</span>
+      <CheckoutTable />
+      <span>
+        <span>R$ </span>
+        <span
+          data-testid="customer_checkout__element-order-total-price"
+        >
+          {priceFormat(getTotalPrice())}
+        </span>
+      </span>
+    </>
   );
 }
 
