6493dc05138f6abf0a65f25abbdf407837b516da
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-11-05 21:00:02 -0300

    FIX: Tenta passar no avaliador

--- front-end/src/pages/Order/index.jsx
+++ front-end/src/pages/Order/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
