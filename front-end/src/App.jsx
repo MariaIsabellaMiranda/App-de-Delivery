53d75984a55d1df6b15f4366f2a264d8d644f2c0
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-11-08 18:40:23 -0300

    FIX: Tenta corrigir requisitos 32-35

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,8 +9,8 @@ import Checkout from './pages/Checkout';
 import Order from './pages/Order';
 import Orders from './pages/Orders';
 import AdminManage from './pages/AdminManage';
-// import './App.css';
-// import './colors/colors.css';
+import './App.css';
+import './colors/colors.css';
 
 function App({ status, role }) {
   const routes = {
