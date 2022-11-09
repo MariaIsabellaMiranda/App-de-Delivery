a39ec94288985e495853c760bcf9f8f94bb6b6dd
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-09 17:39:12 -0300

    test: Testes página seller/orders

---
+++ front-end/src/tests/mocks/responseMocks/sellerOrdersMock.js
@@ -0,0 +1,19 @@
+const sellerOrdersMock = [
+  {
+    id: 1,
+    totalPrice: '64.69',
+    deliveryAddress: 'Rua José',
+    deliveryNumber: '110',
+    saleDate: '2022-11-09T05:28:07.000Z',
+    status: 'Pendente',
+  }, {
+    id: 2,
+    totalPrice: '31.25',
+    deliveryAddress: 'Rua Visconde',
+    deliveryNumber: '1232',
+    saleDate: '2022-11-10T08:02:15.000Z',
+    status: 'Preparando',
+  },
+];
+
+module.exports = sellerOrdersMock;
